1bab3a6592154cde24d368187f9be4144e7efee6
MD AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2023-10-18 22:07:05 +0600

    Add files via upload

---
+++ polymorphism with pointer.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+ 
+ class person 
+ {
+    public:
+        void display()
+        {
+            cout<< "I am a person " <<endl;
+        }
+ };
+
+  class student : public person 
+ {
+    public:
+        void display()
+        {
+            cout<< "I am a student " <<endl;
+        }
+ };
+
+  class teacher : public person
+ {
+    public:
+        void display()
+        {
+            cout<< "I am a teacher " <<endl;
+        }
+ };
+
+ int main()
+ {
+    person *p;
+    student s;
+    teacher t;
+
+    p = & s;
+    s.display();
+
+    p = & t;
+    t.display();
+ }
